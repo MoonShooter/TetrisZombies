09215ee3abfa9c7a0d47a80b2d71edab45f3cecb
Nicholas <moonshoter10@gmail.com>
2018-12-02 11:45:43 -0500

    Added basic AI plus a bunch of textures and a little more details on the first map for the zombies to go around

---
+++ Assets/Scripts/AI_Pathfinder.cs
@@ -0,0 +1,48 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Pathfinder : MonoBehaviour {
+
+    public Transform target;
+
+    Seeker seeker;
+    Path path;
+    int currentWaypoint;
+
+	// Use this for initialization
+	void Start () {
+        seeker = GetComponent<Seeker>();
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+	}
+	
+    public void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+        else
+        {
+            Debug.Log(p.error);
+        }
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (path == null)
+        {
+            return;
+        }
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+
+        transform.position = path.vectorPath[currentWaypoint];
+        currentWaypoint++;
+	}
+}
---
+++ Assets/Scripts/M9_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M9_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("Fire");
+            GetComponent<Animation>().Play("Gunshot");
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("Switch");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            print("Alt Camera");
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            print("Tetris");
+        }
+		
+	}
+}
