d2004fab8e2225de3477e64b605e62beebb0154a
aflayton <aflayton@gmail.com>
2018-12-03 12:22:38 -0500

    Game Loader
    Added functionality to menus

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadGame()
+    {
+		Debug.Log("clicked");
+        SceneManager.LoadScene("_Scene_Main");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
